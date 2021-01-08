bf8374581a7727b1cce47270ed9c0ced553a3e6f
Kristofer <skipperk@interia.pl>
2021-01-08 22:24:45 +0100

    część 2

--- KolkoIKrzyzyk/Form1.Designer.cs
+++ KolkoIKrzyzyk/Form1.Designer.cs
@@ -38,103 +38,125 @@ namespace KolkoIKrzyzyk
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.White;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button1.Location = new System.Drawing.Point(82, 53);
+            this.button1.Location = new System.Drawing.Point(13, 9);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 100);
             this.button1.TabIndex = 0;
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.White;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button2.Location = new System.Drawing.Point(188, 53);
+            this.button2.Location = new System.Drawing.Point(119, 9);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 100);
             this.button2.TabIndex = 1;
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
+            this.button3.BackColor = System.Drawing.Color.White;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button3.Location = new System.Drawing.Point(294, 53);
+            this.button3.Location = new System.Drawing.Point(225, 9);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(100, 100);
             this.button3.TabIndex = 2;
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
+            this.button4.BackColor = System.Drawing.Color.White;
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button4.Location = new System.Drawing.Point(82, 159);
+            this.button4.Location = new System.Drawing.Point(13, 115);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(100, 100);
             this.button4.TabIndex = 3;
-            this.button4.UseVisualStyleBackColor = true;
+            this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
+            this.button5.BackColor = System.Drawing.Color.White;
             this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button5.Location = new System.Drawing.Point(188, 159);
+            this.button5.Location = new System.Drawing.Point(119, 115);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(100, 100);
             this.button5.TabIndex = 4;
-            this.button5.UseVisualStyleBackColor = true;
+            this.button5.UseVisualStyleBackColor = false;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
+            this.button6.BackColor = System.Drawing.Color.White;
             this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button6.Location = new System.Drawing.Point(294, 159);
+            this.button6.Location = new System.Drawing.Point(225, 115);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(100, 100);
             this.button6.TabIndex = 5;
-            this.button6.UseVisualStyleBackColor = true;
+            this.button6.UseVisualStyleBackColor = false;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
+            this.button7.BackColor = System.Drawing.Color.White;
             this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button7.Location = new System.Drawing.Point(82, 265);
+            this.button7.Location = new System.Drawing.Point(13, 221);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(100, 100);
             this.button7.TabIndex = 6;
-            this.button7.UseVisualStyleBackColor = true;
+            this.button7.UseVisualStyleBackColor = false;
             this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
+            this.button8.BackColor = System.Drawing.Color.White;
             this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button8.Location = new System.Drawing.Point(188, 265);
+            this.button8.Location = new System.Drawing.Point(119, 221);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(100, 100);
             this.button8.TabIndex = 7;
-            this.button8.UseVisualStyleBackColor = true;
+            this.button8.UseVisualStyleBackColor = false;
             this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
+            this.button9.BackColor = System.Drawing.Color.White;
             this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button9.Location = new System.Drawing.Point(294, 265);
+            this.button9.Location = new System.Drawing.Point(225, 221);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(100, 100);
             this.button9.TabIndex = 8;
-            this.button9.UseVisualStyleBackColor = true;
+            this.button9.UseVisualStyleBackColor = false;
             this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button10.Location = new System.Drawing.Point(13, 328);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(312, 55);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "RESET";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(473, 450);
+            this.ClientSize = new System.Drawing.Size(337, 395);
+            this.Controls.Add(this.button10);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
@@ -144,6 +166,8 @@ namespace KolkoIKrzyzyk
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -161,6 +185,7 @@ namespace KolkoIKrzyzyk
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
     }
 }
 
--- KolkoIKrzyzyk/Form1.cs
+++ KolkoIKrzyzyk/Form1.cs
@@ -25,49 +25,93 @@ namespace KolkoIKrzyzyk
                 return "X";
             return "O";
         }
+        public System.Drawing.Color DetermineColor(String symbol)
+        {
+            if(symbol.Equals("O"))
+            {
+                return System.Drawing.Color.Aqua;
+            } else if(symbol.Equals("X"))
+            {
+                return System.Drawing.Color.Chartreuse;
+            }
+            return System.Drawing.Color.White;
+        }
         public void ChechForWinner()
         {
             for(int i=0; i <8; i++)
             {
                 String combination = "";
+                int one = 0, two = 0, three = 0;
+
                 switch(i)
                 {
                     case 0:
                         combination = gameBoard[0] + gameBoard[4] + gameBoard[8];
+                        one = 0;
+                        two = 4;
+                        three = 8;
                         break;
                     case 1:
                         combination = gameBoard[2] + gameBoard[4] + gameBoard[6];
+                        one = 2;
+                        two = 4;
+                        three = 6;
                         break;
                     case 2:
                         combination = gameBoard[0] + gameBoard[1] + gameBoard[2];
+                        one = 0;
+                        two = 1;
+                        three = 2;
                         break;
                     case 3:
                         combination = gameBoard[3] + gameBoard[4] + gameBoard[5];
+                        one = 3;
+                        two = 4;
+                        three = 5;
                         break;
                     case 4:
                         combination = gameBoard[6] + gameBoard[7] + gameBoard[8];
+                        one = 6;
+                        two = 7;
+                        three = 8;
                         break;
                     case 5:
                         combination = gameBoard[0] + gameBoard[3] + gameBoard[6];
+                        one = 0;
+                        two = 3;
+                        three = 6;
                         break;
                     case 6:
                         combination = gameBoard[1] + gameBoard[4] + gameBoard[7];
+                        one = 1;
+                        two = 4;
+                        three = 7;
                         break;
                     case 7:
                         combination = gameBoard[2] + gameBoard[5] + gameBoard[8];
+                        one = 2;
+                        two = 5;
+                        three = 8;
                         break;
                 }
 
-                ChechDraw();
+                
 
                 if(combination.Equals("OOO"))
                 {
-                    Reset();
+                    ChangeColor(one);
+                    ChangeColor(two);
+                    ChangeColor(three);
                     MessageBox.Show("O wygrało grę!", "Mamy zwycięzce", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                 } else if (combination.Equals("XXX"))
                 {
-                    Reset();
+                    ChangeColor(one);
+                    ChangeColor(two);
+                    ChangeColor(three);
                     MessageBox.Show("X wygrało grę!", "mamy zwycięzce", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                } else
+                {
+                    ChechDraw();
                 }
             }
         }
@@ -81,12 +125,43 @@ namespace KolkoIKrzyzyk
                     counter++;
                 if(counter == 9)
                 {
-                    Reset();
                     MessageBox.Show("Remis!", "Brak zwycięzcy", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                 }
             }
         }
-
+        public void ChangeColor(int number)
+        {
+            switch(number)
+            {
+                case 0:
+                    button1.BackColor = System.Drawing.Color.Red;
+                    break;
+                case 1:
+                    button2.BackColor = System.Drawing.Color.Red;
+                    break;
+                case 2:
+                    button3.BackColor = System.Drawing.Color.Red;
+                    break;
+                case 3:
+                    button4.BackColor = System.Drawing.Color.Red;
+                    break;
+                case 4:
+                    button5.BackColor = System.Drawing.Color.Red;
+                    break;
+                case 5:
+                    button6.BackColor = System.Drawing.Color.Red;
+                    break;
+                case 6:
+                    button7.BackColor = System.Drawing.Color.Red;
+                    break;
+                case 7:
+                    button8.BackColor = System.Drawing.Color.Red;
+                    break;
+                case 8:
+                    button9.BackColor = System.Drawing.Color.Red;
+                    break;
+            }
+        }
         public void Reset()
         {
             currentTurn = 0;
@@ -99,6 +174,15 @@ namespace KolkoIKrzyzyk
             button7.Text = "";
             button8.Text = "";
             button9.Text = "";
+            button1.BackColor = System.Drawing.Color.White;
+            button2.BackColor = System.Drawing.Color.White;
+            button3.BackColor = System.Drawing.Color.White;
+            button4.BackColor = System.Drawing.Color.White;
+            button5.BackColor = System.Drawing.Color.White;
+            button6.BackColor = System.Drawing.Color.White;
+            button7.BackColor = System.Drawing.Color.White;
+            button8.BackColor = System.Drawing.Color.White;
+            button9.BackColor = System.Drawing.Color.White;
             gameBoard = new string[9];
         }
 
@@ -106,6 +190,8 @@ namespace KolkoIKrzyzyk
         {
             currentTurn++;
             gameBoard[0] = ReturneSymble(currentTurn);
+            Color buttonColor = DetermineColor(gameBoard[0]);
+            button1.BackColor = buttonColor;
             button1.Text = gameBoard[0];
             ChechForWinner();
         }
@@ -114,6 +200,8 @@ namespace KolkoIKrzyzyk
         {
             currentTurn++;
             gameBoard[1] = ReturneSymble(currentTurn);
+            Color buttonColor = DetermineColor(gameBoard[1]);
+            button2.BackColor = buttonColor;
             button2.Text = gameBoard[1];
             ChechForWinner();
         }
@@ -122,6 +210,8 @@ namespace KolkoIKrzyzyk
         {
             currentTurn++;
             gameBoard[2] = ReturneSymble(currentTurn);
+            Color buttonColor = DetermineColor(gameBoard[2]);
+            button3.BackColor = buttonColor;
             button3.Text = gameBoard[2];
             ChechForWinner();
         }
@@ -130,6 +220,8 @@ namespace KolkoIKrzyzyk
         {
             currentTurn++;
             gameBoard[3] = ReturneSymble(currentTurn);
+            Color buttonColor = DetermineColor(gameBoard[3]);
+            button4.BackColor = buttonColor;
             button4.Text = gameBoard[3];
             ChechForWinner();
         }
@@ -138,6 +230,8 @@ namespace KolkoIKrzyzyk
         {
             currentTurn++;
             gameBoard[4] = ReturneSymble(currentTurn);
+            Color buttonColor = DetermineColor(gameBoard[4]);
+            button5.BackColor = buttonColor;
             button5.Text = gameBoard[4];
             ChechForWinner();
         }
@@ -146,6 +240,8 @@ namespace KolkoIKrzyzyk
         {
             currentTurn++;
             gameBoard[5] = ReturneSymble(currentTurn);
+            Color buttonColor = DetermineColor(gameBoard[5]);
+            button6.BackColor = buttonColor;
             button6.Text = gameBoard[5];
             ChechForWinner();
         }
@@ -154,6 +250,8 @@ namespace KolkoIKrzyzyk
         {
             currentTurn++;
             gameBoard[6] = ReturneSymble(currentTurn);
+            Color buttonColor = DetermineColor(gameBoard[6]);
+            button7.BackColor = buttonColor;
             button7.Text = gameBoard[6];
             ChechForWinner();
         }
@@ -162,6 +260,8 @@ namespace KolkoIKrzyzyk
         {
             currentTurn++;
             gameBoard[7] = ReturneSymble(currentTurn);
+            Color buttonColor = DetermineColor(gameBoard[7]);
+            button8.BackColor = buttonColor;
             button8.Text = gameBoard[7];
             ChechForWinner();
         }
@@ -170,8 +270,15 @@ namespace KolkoIKrzyzyk
         {
             currentTurn++;
             gameBoard[8] = ReturneSymble(currentTurn);
+            Color buttonColor = DetermineColor(gameBoard[8]);
+            button9.BackColor = buttonColor;
             button9.Text = gameBoard[8];
             ChechForWinner();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Reset();
         }
     }
 }
